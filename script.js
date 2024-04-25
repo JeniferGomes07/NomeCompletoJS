cd5638940a588d02e0080ec36542d5aa7a9927b7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-25 20:26:33 -0300

    exercicio concluido

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Qual o seu nome?");
+ 
+let sobrenome = prompt("Qual o seu sobrenome?");
+
+alert("Seu nome é: " + nome + " " + sobrenome);
+ 
+alert("Nome de catálogo: " + sobrenome.toUpperCase() + " , " + nome);
+
+
